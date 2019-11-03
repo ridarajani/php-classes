b5f2cfbcfa24a86d2438afe65bf399b7823eb176
ridarajani <ridarajani125@gmail.com>
2019-11-03 07:03:38 -0800

    blabla

---
+++ CodeIgniter-assignment-3/application/core/MY_Model.php
@@ -0,0 +1,16 @@
+<?php
+    class MY_Model extends CI_Model{
+
+        public function create_insert(){
+            $data = [];               
+        }
+        public function read_select(){
+            $data = [];
+        }
+        public function update(){
+            $data = [];
+        }
+        public function delete(){
+            $data = [];
+        }
+    }
